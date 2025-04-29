79293aa6b71c8c4207414e39335941a3891ef197
Dmitriy Kazakov <79647976794@ya.ru>
2025-04-29 15:45:01 +0300

    add processing.py

---
+++ src/processing.py
@@ -0,0 +1,8 @@
+def filter_by_state(list_of_dicts: list, state_for_key: str = "EXECUTED") -> list:
+
+    pass
+
+
+def sort_by_date(list_of_dicts: list, ascending: bool = True) -> list:
+
+    pass
