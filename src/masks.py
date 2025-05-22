72b1b27e5340386e40ffdf83c46069a044629bb8
Dmitriy Kazakov <79647976794@ya.ru>
2025-05-22 18:18:28 +0300

    homework_10_2

--- src/masks.py
+++ src/masks.py
@@ -1,10 +1,14 @@
 def get_mask_card_number(card_number: str) -> str:
     """Функция маскирует номер карты, заменяя цифры на '*'"""
-    card_number_mask = card_number[:-12] + " " + card_number[-12:-10] + "** **** " + card_number[-4:]
-    return card_number_mask
+    if card_number.isdigit() == True and len(card_number) == 16:
+        return card_number[:-12] + " " + card_number[-12:-10] + "** **** " + card_number[-4:]
+    else:
+        return "Не верный номер!"
 
 
 def get_mask_account(account_number: str) -> str:
     """Функция маскирует номер счета, заменяя цифры на '*'"""
-    account_number_mask = "**" + account_number[-4:]
-    return account_number_mask
+    if account_number.isdigit() == True and len(account_number) == 20:
+        return "**" + account_number[-4:]
+    else:
+        return "Не верный номер!"
